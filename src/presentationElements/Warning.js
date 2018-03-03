a8598f83fea68b88c238438381816f382dc92837
Denis Rylow <denisrylow@gmail.com>
2018-03-03 10:19:56 +0300

    Cleaning up.

--- src/presentationElements/Warning.js
+++ src/presentationElements/Warning.js
@@ -32,38 +32,3 @@ class Warning extends Component {
 }
 
 export default Warning;
-{/*<strong>Внимание!</strong> В подсвеченных красным полях содержатся неправильные значения. 
-    const { errorFlags } = this.props;
-    var errorTrigger = false;
-    var message = '';
-    if (errorFlags['email'] != 'valid') {
-      message += ' Неправильный формат электронного адреса.';
-      errorTrigger = true;
-    };
-    if (errorFlags['name'] != 'valid') {
-      message += ' Неправильный формат имени. Имя должно начинаться с большой буквы и быть длинной не менее 4 символов.';
-      errorTrigger = true;
-    };
-
-  constructor(props) {
-    super(props);
-    this.errorFlags = props.errorFlags;
-    this.errorTrigger = false;
-    this.message = 'Ошибка при вводе данных.';
-    if (this.errorFlags['email'] != 'valid') {
-      this.message += ' Неправильный формат электронного адреса.';
-      this.errorTrigger = true;
-    };
-    if (this.errorFlags['name'] != 'valid') {
-      this.message += ' Неправильный формат имени. Имя должно начинаться с большой буквы и быть длинной не менее 4 символов.';
-      this.errorTrigger = true;
-    };
-    console.log('Warning header created. Trigger is ', this.errorTrigger, ', props:', props);
-  }
-  
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('Warning component checks for updates.', nextProps.errorFlags != this.errorFlags);
-    return nextProps.errorFlags != this.errorFlags;  
-  }
-
-  */}
